7f5ae250fec5507330b23573a5bcb8534eadd658
vootlav <varun.vootla@slu.edu>
2025-03-09 00:38:33 +0000

    I have updated the code with all the instructions given in the lab 8 - Varun Vootla

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const api = require('./api')
 const middleware = require('./middleware')
 const bodyParser = require('body-parser')
 
+
 // Set the port
 const port = process.env.PORT || 3000
 
---
+++ tests/app.test.js
@@ -0,0 +1,30 @@
+// tests/app.test.js
+const request = require('supertest');
+const app = require('../app.js');
+
+
+// tests/app.test.js
+describe('The Express Server', () => {
+    beforeAll(done => {
+      done()
+    })
+  
+    test('should return response', async () => {
+      const res = await request(app)
+        .get('/');
+      expect(res.statusCode).toEqual(200);
+  
+    });
+  
+    test('should respond at /products', async () => {
+      const res = await request(app)
+        .get('/products')
+      expect(res.statusCode).toEqual(200);
+    });
+
+    test('should respond at /orders', async () => {
+        const res = await request(app)
+          .get('/orders')
+        expect(res.statusCode).toEqual(200);
+      });
+  });
